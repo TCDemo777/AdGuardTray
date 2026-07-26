acaa6268f031c84f420d3acc058aebca99a5a6f4
Tristan Caldwell <tristan.caldwell@tuta.io>
2026-07-26 17:23:54 +0100

    Add client model

---
+++ AdGuardTray/Models/ClientInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace AdGuardTray.Models
+{
+    public class ClientInfo
+    {
+        public string Name { get; set; } = "-";
+
+        public string IpAddress { get; set; } = "-";
+
+        public string MacAddress { get; set; } = "-";
+
+        public int TotalQueries { get; set; }
+
+        public int BlockedQueries { get; set; }
+
+        public double BlockRate =>
+            TotalQueries == 0
+                ? 0
+                : (double)BlockedQueries / TotalQueries * 100;
+
+        public string LastSeen { get; set; } = "-";
+    }
+}
