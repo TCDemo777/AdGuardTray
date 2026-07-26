923014d7e3773d85d1ae8e72f1ed8b4675dac3fb
Tristan Caldwell <tristan.caldwell@tuta.io>
2026-07-26 13:27:03 +0100

    Add analytics view

---
+++ AdGuardTray/Views/OverviewView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AdGuardTray.Views
+{
+    public partial class OverviewView : UserControl
+    {
+        public OverviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
